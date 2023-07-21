89d0c83a1d34e14dbd2923b9cb6c0f8e65b30e8c
Debian <bitnami@ip-172-26-12-249.us-east-2.compute.internal>
2023-07-21 13:28:15 +0000

    add location field plugin

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,38 +1,43 @@
-module.exports = ({ env }) => ([
-  'strapi::errors',
+module.exports = ({ env }) => [
+  "strapi::errors",
   //'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
   {
-    name: 'strapi::security',
+    name: "strapi::security",
     config: {
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          'connect-src': ["'self'", 'https:'],
-          'img-src': [
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
             "'self'",
-            'data:',
-            'blob:',
-            'dl.airtable.com',
-            `${env('S3_BUCKET_NAME')}.s3.amazonaws.com`,
+            "data:",
+            "blob:",
+            `https://${env("S3_BUCKET_NAME")}.s3.${env("S3_REGION")}.amazonaws.com`,
+            `https://${env("S3_BUCKET_NAME")}.s3.amazonaws.com`,
           ],
-          'media-src': [
-            "'self'",
-            'data:',
-            'blob:',
-            'dl.airtable.com',
-            `${env('S3_BUCKET_NAME')}.s3.amazonaws.com`,
-          ],
+          "media-src": ["'self'", "data:", "blob:"],
+          "script-src": ["'self'", "'unsafe-inline'", "maps.googleapis.com"],
           upgradeInsecureRequests: null,
         },
       },
     },
-  }
-]);
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  {
+    name: "strapi::body",
+    config: {
+      formLimit: "256mb", // modify form body
+      jsonLimit: "256mb", // modify JSON body
+      textLimit: "256mb", // modify text body
+      formidable: {
+        maxFileSize: 250 * 1024 * 1024, // multipart data, modify here limit of uploaded file size
+      },
+    },
+  },
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+];
--- config/plugins.js
+++ config/plugins.js
@@ -1,22 +1,32 @@
 module.exports = ({ env }) => ({
 	upload: {
 		config: {
-			provider: 'aws-s3',
+			provider: "aws-s3",
 			providerOptions: {
 				s3Options: {
-					accessKeyId: env('S3_ACCESS_KEY_ID'),
-					secretAccessKey: env('S3_SECRET_ACCESS_KEY'),
-					region: env('S3_REGION'),
+					accessKeyId: env("S3_ACCESS_KEY_ID"),
+					secretAccessKey: env("S3_SECRET_ACCESS_KEY"),
+					region: env("S3_REGION"),
 					params: {
-						Bucket: env('S3_BUCKET_NAME'),
+						Bucket: env("S3_BUCKET_NAME"),
 					},
-				}
+				},
 			},
 			actionOptions: {
 				upload: { ACL: "public-read" },
 				uploadStream: { ACL: "public-read" },
 				delete: {},
 			},
+		},
+	},
+	"location-field": {
+		enabled: true,
+		config: {
+			fields: ["photo", "rating"], // optional
+			// You need to enable "Autocomplete API" and "Places API" in your Google Cloud Console
+			googleMapsApiKey: env("GOOGLE_MAPS_API_KEY"),
+			// See https://developers.google.com/maps/documentation/javascript/reference/places-autocomplete-service#AutocompletionRequest
+			autocompletionRequestOptions: {},
 		},
 	},
 });
